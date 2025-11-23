513d96a23acb70270c4e7d7fe06f5d45e89dae0e
robertbrewer <robertbrewer@robertbrewer-HP-Pavilion-Desktop-PC-570-p0xx>
2025-11-23 12:15:01 -0800

    Corrected HoursWorkedPanel layout

--- timecard/src/main/java/com/timecard/timecard/HoursWorkedPanel.java
+++ timecard/src/main/java/com/timecard/timecard/HoursWorkedPanel.java
@@ -23,11 +23,14 @@ import javax.swing.SpringLayout;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.EventQueue;
 
 import javax.swing.JTextField;
+import java.awt.FlowLayout;
 
 public class HoursWorkedPanel extends JPanel {
 
+	
 	private static final long serialVersionUID = 1L;
 	private JTextField textField_MinHours;
 	private JTextField textField_MaxHours;
@@ -37,7 +40,7 @@ public class HoursWorkedPanel extends JPanel {
 	public HoursWorkedPanel(final MySQLConnect mydatabase, Boolean dStatus) {
 		SpringLayout springLayout = new SpringLayout();
 		setLayout(springLayout);
-		setPreferredSize(new Dimension(450, 178)); // (Width, Height)
+		//setPreferredSize(new Dimension(450, 178)); // (Width, Height)
 		setBackground(Color.PINK);
         /*
 		JLabel lblBeginDate = new JLabel("BEGIN DATE");
@@ -105,10 +108,10 @@ public class HoursWorkedPanel extends JPanel {
 		springLayout.putConstraint(SpringLayout.NORTH, lblMinHours, 0, SpringLayout.NORTH, lblBeginDate);
 		add(lblMinHours);
 
-		JLabel lblMinHours_1 = new JLabel("MAX HOURS");
-		springLayout.putConstraint(SpringLayout.NORTH, lblMinHours_1, 0, SpringLayout.NORTH, lblBeginDate);
-		springLayout.putConstraint(SpringLayout.EAST, lblMinHours_1, -47, SpringLayout.EAST, this);
-		add(lblMinHours_1);
+		JLabel lblMaxHours = new JLabel("MAX HOURS");
+		springLayout.putConstraint(SpringLayout.NORTH, lblMaxHours, 0, SpringLayout.NORTH, lblBeginDate);
+		springLayout.putConstraint(SpringLayout.EAST, lblMaxHours, 100, SpringLayout.EAST, lblMinHours);
+		add(lblMaxHours);
 
 		textField_MinHours = new JTextField();
 		springLayout.putConstraint(SpringLayout.NORTH, textField_MinHours, 6, SpringLayout.SOUTH, lblMinHours);
@@ -119,16 +122,16 @@ public class HoursWorkedPanel extends JPanel {
 
 		textField_MaxHours = new JTextField();
 		springLayout.putConstraint(SpringLayout.EAST, textField_MinHours, -29, SpringLayout.WEST, textField_MaxHours);
-		springLayout.putConstraint(SpringLayout.NORTH, textField_MaxHours, 6, SpringLayout.SOUTH, lblMinHours_1);
+		springLayout.putConstraint(SpringLayout.NORTH, textField_MaxHours, 6, SpringLayout.SOUTH, lblMaxHours);
 		springLayout.putConstraint(SpringLayout.WEST, textField_MaxHours, 346, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.EAST, textField_MaxHours, -26, SpringLayout.EAST, this);
+		//springLayout.putConstraint(SpringLayout.EAST, textField_MaxHours, -26, SpringLayout.EAST, this);
 		textField_MaxHours.setColumns(7);
 		add(textField_MaxHours);
 
 		JButton btnResults = new JButton("RESULTS");
-		springLayout.putConstraint(SpringLayout.SOUTH, textField_MinHours, -19, SpringLayout.NORTH, btnResults);
-		springLayout.putConstraint(SpringLayout.NORTH, btnResults, 133, SpringLayout.NORTH, this);
-		springLayout.putConstraint(SpringLayout.EAST, btnResults, -164, SpringLayout.EAST, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, textField_MinHours, -27, SpringLayout.NORTH, btnResults);
+		springLayout.putConstraint(SpringLayout.WEST, btnResults, 175, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, btnResults, -10, SpringLayout.SOUTH, this);
 		add(btnResults);
 
 		
@@ -207,11 +210,20 @@ public class HoursWorkedPanel extends JPanel {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	
+        HoursWorkedPanel hoursworkedpanel = new HoursWorkedPanel(null, null);
+		JFrame myFrame = new JFrame();
+		myFrame.setResizable(true);
+		myFrame.setTitle("Queried Hours");
+		myFrame.getContentPane().setLayout(new BorderLayout());
+		myFrame.setSize(500,250);
+		myFrame.getContentPane().add(hoursworkedpanel);
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setVisible(true);
 
+			  
 
 	}
-
-
 
 
 }
